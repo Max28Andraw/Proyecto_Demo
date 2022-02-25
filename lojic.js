a61b522f9e0f616b43cff43b87c675d431261e7b
Max28Andraw <andrade28ivan@hotmail.com>
2022-02-25 15:27:59 -0600

    Nuevo Curso

--- lojic.js
+++ lojic.js
@@ -1,1 +1,22 @@
-alert("Que paso Raza");
+function valor(){
+    const input = document.getElementById("inputCuadrado");
+    return value = input.value;
+}
+
+function perimetroCuadrado (){
+    const perimetro = valor() * 4;
+    alert(perimetro + " cm.");
+}
+function areaCuadrado (){
+    const area = valor() * valor();
+    alert(area + " cm^2.");
+}
+
+function altura(){
+    const base = document.getElementById("inputTrianguloBase");
+    const b = base.value;
+    const altura = document.getElementById("inputTrianguloLados");
+    const a = altura.value;
+    const c = (Math.sqrt((a * a) - ((b * b) / 4)));
+    alert(c + " cm");
+}
